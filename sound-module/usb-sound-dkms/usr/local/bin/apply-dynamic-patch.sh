8b4bd1edcc328ae6c6a3ea90b7aafa4c7f2ce345
Antho91 <basverheule1991@gmail.com>
2024-10-09 10:12:48 +0200

    Make the patch more readable

--- sound-module/usb-sound-dkms/usr/local/bin/apply-dynamic-patch.sh
+++ sound-module/usb-sound-dkms/usr/local/bin/apply-dynamic-patch.sh
@@ -3,5 +3,13 @@
 # File where the changes should be applied
 FILE="mixer.c"
 
-# Look for the line with USB_ID(0x0d8c, 0x0103) and remove the corresponding block
-sed -i '/case USB_ID(0x0d8c, 0x0103):/,/break;/d' $FILE
+# Check if the specific USB_ID block exists before attempting to remove it
+if grep -q "case USB_ID(0x0d8c, 0x0103):" "$FILE"; then
+  echo "Removing block for USB_ID(0x0d8c, 0x0103) in $FILE"
+  
+  # Use sed to remove the block starting from "case USB_ID(0x0d8c, 0x0103):" to the next "break;"
+  # This approach is dynamic and doesn't depend on line numbers.
+  sed -i '/case USB_ID(0x0d8c, 0x0103):/,/break;/d' "$FILE"
+else
+  echo "No block found for USB_ID(0x0d8c, 0x0103) in $FILE"
+fi
